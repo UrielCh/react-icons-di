72fcbcf4b7088f407ca58f5e560e6a93d1762ea1
urielch <uriel.chemouni@gmail.com>
2023-05-31 18:09:06 +0300

    fix dual @

--- mod.ts
+++ mod.ts
@@ -17,8 +17,8 @@
  *   "imports": {
  *     "preact":  "https://esm.sh/preact@10.15.1",
  *     "preact/": "https://esm.sh/preact@10.15.1/",
- *     "react-icons/di":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-di@1.0.4/mod.ts",
- *     "react-icons/di/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-di@1.0.4/ico/",
+ *     "react-icons/di":  "https://cdn.jsdelivr.net/gh/urielch/react-icons-di@1.0.5/mod.ts",
+ *     "react-icons/di/": "https://cdn.jsdelivr.net/gh/urielch/react-icons-di@1.0.5/ico/",
  *   }
  * }
  * ```
@@ -26,7 +26,7 @@
  * ## Direct import sample
  * 
  * ```ts
- * import { DiAndroid } from "https://deno.land/x/react_icons_di@1.0.4/mod.ts"
+ * import { DiAndroid } from "https://deno.land/x/react_icons_di@1.0.5/mod.ts"
  * ```
  * 
  * ## import_map import sample
